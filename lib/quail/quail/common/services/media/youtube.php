99c9903aa389ec62ebb6346ae7f3834ffb3e2a8c
Fenel Joseph <fenel.joseph@ucf.edu>
2016-05-17 12:02:59 -0400

    Test for unavailable YouTube videos implemented, pending QA by myself first, then others

--- lib/quail/quail/common/services/media/youtube.php
+++ lib/quail/quail/common/services/media/youtube.php
@@ -65,6 +65,17 @@ class youtubeService extends mediaService
 	*/
 	function videoUnavailable($link_url)
 	{
+		$Url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=';
+		$api_key = constant( 'GOOGLE_API_KEY' );
+
+		if( $youtube_id = $this->isYouTubeVideo($link_url) ) {
+			$url = $url.$youtube_id.'&key='.$api_key;
+			$response = Request::get($url)->send();
+
+			
+
+		}
+
 		return false;
 	}
 
