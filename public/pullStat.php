4195d59706a6062783859f5f580e6ff55ddbb15b
Jacob Bates <jacob.bates@ucf.edu>
2018-04-11 15:04:12 -0400

    Chunking out error code and adding more stats

--- public/pullStat.php
+++ public/pullStat.php
@@ -23,7 +23,6 @@ require_once(__DIR__.'/../config/settings.php');
 // Sanitize post parameters
 $get_input = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
 
-
 session_start();
 header('Content-type: text/json');
 
@@ -43,58 +42,49 @@ function respond_with_error($http_code, $message)
         'message' => $message,
     ];
 
-    // Set response to 401 (Unauthorized)
     respond_and_die($response, $http_code);
+}
+
+function respond_with_success($message)
+{
+    $response = [
+        'status'  => 'error',
+        'message' => $message,
+    ];
+
+    respond_and_die($response, 200);
 }
 
 // Verify we have the variables we need from the LTI launch
 $expect = ['base_url', 'launch_params', 'is_admin'];
 foreach ($expect as $key) {
     if (empty($_SESSION[$key])) {
-        $response = [
-            'status'  => 'error',
-            'message' => "Missing LTI launch information. Please ensure that your instance of UDOIT is installed to Canvas correctly. Missing: {$key}",
-        ];
-
         // Set response to 401 (Unauthorized)
-        respond_and_die($response, 401);
+        respond_with_error(401, "Missing LTI launch information. Please ensure that your instance of UDOIT is installed to Canvas correctly. Missing: {$key}")
     }
 }
 
 // If Administrator is not found in the user's list of roles, kick them out with an error
 if (!$_SESSION['is_admin']) {
-    $response = [
-        'status'  => 'error',
-        'message' => 'Insufficient permissions to continue.  Please contact your LMS Administrator.',
-    ];
     // Set response to 403 (Forbidden)
-    respond_and_die($response, 403);
+    respond_with_error(403, 'Insufficient permissions to continue.  Please contact your LMS Administrator.')
 }
 
 // Verify we have the minimum GET parameters we need
 if (empty($get_input['stat'])) {
-    $response = [
-        'status'  => 'error',
-        'message' => "Request is missing the stat parameter.  Please contact your administrator.",
-    ];
-
     // Set response to 400 (Bad Request)
-    respond_and_die($response, 400);
+    respond_with_error(400, 'Request is missing the stat parameter.  Please contact your administrator.');
 }
 
 switch ($get_input['stat']) {
     case 'users':
         $results = UdoitStats::instance()->getUsers();
-        if ($results !== false) {
-            $response = [
-                'status' => 'success',
-                'data' => $results,
-            ];
-            respond_and_die($response);
-        } else {
+
+        if ($results === false) {
             respond_with_error(500, "Error retrieving Users from database.");
         }
-        
+
+        respond_with_success($results);
         break;
 
     case 'usergrowth':
@@ -106,14 +96,18 @@ switch ($get_input['stat']) {
             }
         }
 
+        $startDate = new DateTime($get_input['startDate']);
+        $startDate = new DateTime($get_input['endDate']);
+
+        $results = UdoitStats::instance()->countNewUsers($get_input['grain'], $startDate, $endDate);
+
+        if ($results === false) {
+            respond_with_error(500, "Error retrieving Users from database.");
+        }
+
+        respond_with_success($results);
         break;
 
     default:
-        $response = [
-            'status'  => 'error',
-            'message' => "Stat {$get_input['stat']} does not exist.",
-        ];
-
-        // Set response to 400 (Bad Request)
-        respond_and_die($response, 400);
+        respond_with_error(400, "Stat {$get_input['stat']} does not exist.")
 }
