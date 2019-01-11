53fbe68a436726a1437982808dbc311983371244
Jacob Bates <jacob.bates@ucf.edu>
2019-01-10 21:49:32 -0500

    Add function to escape selector for view error source

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -20,6 +20,11 @@
 var progressTimer = null;
 var $doc = $(document); // hold a jquery doc reference
 
+/* Escapes special characters for use in jquery selectors. */
+function escapeSelector(sel){
+	return sel.replace( /(:|\.|\[|\]|,|=|@)/g, "\\$1" );
+}
+
 /* Fades out and destroys the popup window and background. */
 function killButton(callback) {
 	var $popup = $('#popup');
@@ -355,7 +360,7 @@ $doc.ready(function() {
 
 	// view error source
 	$doc.on('click', '.viewError', function(e) {
-		let errorId = e.target.dataset.error;
+		let errorId = escapeSelector(e.target.dataset.error);
 		let $error = $('#'+errorId);
 
 		$(this).addClass('hidden');
