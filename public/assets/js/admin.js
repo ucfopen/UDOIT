886434a7506499b979688b1b84d8c342bb8f5150
Thomas Whitaker <thomas.whitaker@ucf.edu>
2019-02-28 11:17:00 -0500

    Rearranged some logic

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -222,15 +222,15 @@ function generateLinksUsers(total_pages) {
 		let link_page = Math.ceil(user_page_index / 7) - 1;
 		if(link_page == 0) {
 			makeLinks(1, 7,  navigation);
+			makeLinksEnd(8, total_pages, navigation);
 		} else {
 			makeLinksBeginning((link_page * 7) - 1 , navigation);
-		}
-
-		if(user_page_index > (total_pages - 7)) {
-			makeLinks(total_pages - 6, total_pages, navigation);
-		} else {
-			makeLinks((link_page * 7) + 1, (link_page + 1) * 7, navigation);
-			makeLinksEnd((link_page * 7) + 8, total_pages, navigation);
+			if(user_page_index > (total_pages - 7)) {
+				makeLinks(total_pages - 6, total_pages, navigation);
+			} else {
+				makeLinks((link_page * 7) + 1, (link_page + 1) * 7, navigation);
+				makeLinksEnd((link_page * 7) + 8, total_pages, navigation);
+			}
 		}
 	}
 
