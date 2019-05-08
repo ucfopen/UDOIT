0eab3306567040081ffb3160d2e3e77fc80d9093
Thomas Whitaker <thomas.whitaker@ucf.edu>
2019-05-08 13:07:29 -0400

    Fixed a jquery selector

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -493,7 +493,7 @@ $doc.ready(function() {
 		if (type == "imgAltIsDifferent" || type == "imgAltIsTooLong" || type == "imgHasAlt" || type == "imgNonDecorativeHasAlt" || type == "imgHasAltDeco") {
 			var $input = $(this).find('input[name="newcontent"]');
 			var $imgSrc = $(this).find('input[name="errorhtml"]');
-			var makeDeco = $(this).find('input[name="makedeco"]').value == 'true';
+			var makeDeco = $(this).find('input[name="makedeco"]').is(':checked');
 			if(!makeDeco) {
 				if ($input.val().trim() == ''){
 					valid = false;
