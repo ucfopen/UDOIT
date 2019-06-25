dd00d621366974c0dc3663c561b786a498f8b1a8
Ethan Finlay <ethan.finlay@ucf.edu>
2019-06-25 13:13:21 -0400

    reverting test changes, deploy is working

--- config/settings.php
+++ config/settings.php
@@ -52,6 +52,6 @@ header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 
 // Messages
-$udoit_welcome_message = 'Is this thing on?';
+$udoit_welcome_message = 'The Universal Design Online content Inspection Tool (U<strong>DO</strong>IT) was created by the Center for Distributed Learning at the University of Central Florida. U<strong>DO</strong>IT will scan your course content, generate a report and provide instructions on how to correct accessibility issues. Funding for U<strong>DO</strong>IT was provided by a Canvas Grant awarded in 2014.';
 
-$udoit_disclaimer_message = 'No';
+$udoit_disclaimer_message = 'Please Note: This tool is meant to be used as a guide, not a certification. It only checks for common accessibility issues, and is not comprehensive; a clean report in U<strong>DO</strong>IT does not necessarily mean that your course is fully accessible. Likewise, the tool may indicate a possible accessibility issue where one does not exist.';
