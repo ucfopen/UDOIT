88c2e137569d3d8a837ad432cebe419fd82207cb
Jacob Bates <jacob.bates@ucf.edu>
2019-07-26 10:26:19 -0400

    Add quotes around CSV values

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -169,7 +169,7 @@ function tableToCSV(html, filename) {
     for (var i = 0; i < rows.length; i++) {
 		var row = [], cols = rows[i].querySelectorAll("td, th");
         for (var j = 0; j < cols.length; j++)
-            row.push(cols[j].innerText);
+            row.push('"' + cols[j].innerText + '"');
 		csv.push(row.join(","));
 	}
     downloadCSV(csv.join("\n"), filename);
