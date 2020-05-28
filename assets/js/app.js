395cb95a65c29d858deb8e4aa8576a7c5e8a8204
Ethan  Finlay <ethanfinlay@Ethans-MacBook-Pro.local>
2020-05-28 12:03:35 -0400

    initial setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from 'react';
+import ReactDOM from 'react-dom'
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    )
+  }
+}
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('tool', './assets/js/app.js')
+    // .addEntry('tool', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -58,6 +58,9 @@ Encore
 
     // enables .js and .jsx support
     .enableReactPreset()
+
+    // uncomment to define the assets of the project
+    .addEntry('js/app', './assets/js/app.js');
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
