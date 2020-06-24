1933a3387fc81b862283bf7c802a53b3d276b89e
Kurt Volmar <kurtvolmar@MacBook-Air.local>
2020-06-24 15:00:05 -0600

    Get PDF in API. BASIC twig template. New dependencies for PDF tools.

--- src/Controller/ReportsController.php
+++ src/Controller/ReportsController.php
@@ -6,10 +6,17 @@ namespace App\Controller;
 
 use App\Entity\Report;
 use App\Response\ApiResponse;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Knp\Snappy\Pdf;
+use Mpdf\Mpdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\FileinfoMimeTypeGuesser;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Twig\Environment;
 
 /**
  * Class ReportsController
@@ -92,12 +99,11 @@ class ReportsController extends ApiController
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function getPdfReport(Request $request, $courseId, $reportId) {
-        $apiResponse = new ApiResponse();
         try {
             // Check if user has course access
-            if(!$this->userHasCourseAccess($courseId)) {
-                throw new \Exception("You do not have permission to access the specified course.");
-            }
+//            if(!$this->userHasCourseAccess($courseId)) {
+//                throw new \Exception("You do not have permission to access the specified course.");
+//            }
 
             // Get Report
             $repository = $this->getDoctrine()->getRepository(Report::class);
@@ -108,17 +114,48 @@ class ReportsController extends ApiController
                 throw new \Exception(sprintf("Report with ID %s does not exist", $reportId));
             }
 
-            // TODO: Generate PDF Here
+            $html = $this->renderView(
+                    'report.html.twig',
+                        ['report' => $report]
+            );
 
-            $apiResponse->setData(["status" => "This API endpoint is under construction."]);
+            $mPdf = new Mpdf();
+            $mPdf->WriteHTML($html);
+            return new PdfResponse(
+                $mPdf->Output(),
+                array(
+                    'Content-Type'          => 'application/pdf',
+                    'Content-Disposition'   => 'attachment; filename="file.pdf"'
+                )
+            );
         }
         catch(Exception $e) {
+            $apiResponse = new ApiResponse();
             $apiResponse->setData($e->getMessage());
+            $jsonResponse = new JsonResponse($apiResponse);
+            $jsonResponse->setEncodingOptions($jsonResponse->getEncodingOptions() | JSON_PRETTY_PRINT);
+            return $jsonResponse;
         }
-
-        // Construct response
-        $jsonResponse = new JsonResponse($apiResponse);
-        $jsonResponse->setEncodingOptions($jsonResponse->getEncodingOptions() | JSON_PRETTY_PRINT);
-        return $jsonResponse;
     }
+
+
+
+    //            // Get Twig Template from folder path
+//            $folderPath = "/Users/kurtvolmar/Cidi/";
+//            $fileName = "map.pdf";
+//            $filePath = $folderPath . $fileName;
+//            $response = new BinaryFileResponse();
+//            $response->setContent($pdf);
+//            $mimeTypeGuesser = new FileinfoMimeTypeGuesser();
+//            // Guess mimetype or set type manually
+//            if($mimeTypeGuesser->isGuesserSupported()){
+//                $response->headers->set('Content-Type', $mimeTypeGuesser->guessMimeType($filePath));
+//            }else{
+//                $response->headers->set('Content-Type', 'text/plain');
+//            }
+    // Set content disposition inline of the file
+//            $response->setContentDisposition(
+//                ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+//                $fileName
+//            );
 }
