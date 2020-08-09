120631e5175290c2e3474ce6783e45890fb6083b
Ethan  Finlay <ethanfinlay@macbook-pro.attlocal.net>
2020-08-09 00:29:22 -0400

    comments and fixed babel rest spread problem

--- assets/js/Reducers/index.js
+++ assets/js/Reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import issueListReducer from './issueList'
 
+// Combining all of the reducers into one object
 const allReducers = combineReducers({
     issueList: issueListReducer,
 });
--- assets/js/configureStore.js
+++ assets/js/configureStore.js
@@ -3,7 +3,11 @@ import allReducers from './Reducers'
 
 export default function configureStore(params) { 
     const store = createStore(
+        // All the reducers combined from Reducers/index.js
         allReducers,
+        
+        // Enabling Chrome react devtools extension for easier debugging
+        // You will stiil need to download the extension through Chrome
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
