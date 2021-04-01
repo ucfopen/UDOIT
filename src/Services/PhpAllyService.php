388cfd67b10d21b52672bac7ef98469d30c3b6a7
Ethan  Finlay <ethanfinlay@MacBook-Pro.attlocal.net>
2021-04-01 11:51:10 -0400

    making changes based on comments

--- src/Services/PhpAllyService.php
+++ src/Services/PhpAllyService.php
@@ -47,9 +47,9 @@ class PhpAllyService {
         $allRules = $this->phpAlly->getRuleIds();
 
         $envExclusions = $this->getEnvExcludedRules();
-        // $dbExclusions = $this->getDbExcludedRules();
+        $dbExclusions = $this->getDbExcludedRules();
 
-        return array_values(array_diff($allRules, $envExclusions));
+        return array_values(array_diff($allRules, $envExclusions, $dbExclusions));
     }
 
     protected function getEnvExcludedRules()
@@ -60,6 +60,6 @@ class PhpAllyService {
     protected function getDbExcludedRules()
     {
         // TODO: To be implemented with the admin section
-        return ['VideoCaptionsMatchCourseLanguage', 'VideoEmbedCheck', 'VideoProvidesCaptions', 'VideosEmbeddedOrLinkedNeedCaptions'];
+        return [];
     }
 }
