fd32f619e4294fa0c4d8f505d12bfcdc8bd597f4
Chuck Crandall <chuck@cidilabs.com>
2021-04-28 17:02:55 -0600

    Trying to fix JS tests

--- assets/js/__tests__/fixtures/settings.js
+++ assets/js/__tests__/fixtures/settings.js
@@ -2,3 +2,4 @@ import labelsJson from '../../../../translations/en.json'
 
 export const course = { id: 'test' }
 export const labels = labelsJson
+export const institution = { id: 1, lmsDomain: 'cidilabs.instructure.com' }
