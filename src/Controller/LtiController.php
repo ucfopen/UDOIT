157c4d5fd4ad44db779c5910d877ca387152b463
Chuck Crandall <chuck@cidilabs.com>
2021-06-14 14:55:52 -0600

    Updating scopes for Canvas. Removing Cidi Labs Youtube and Vimeo API keys from .env.

--- src/Controller/LtiController.php
+++ src/Controller/LtiController.php
@@ -148,7 +148,7 @@ class LtiController extends AbstractController
         $baseDomain = str_replace('https://', '', $baseUrl);
         $appName = $request->server->get('APP_LTI_NAME');
         $adminName = $request->server->get('ADMIN_LTI_NAME');
-        $platform = 'canvas.instructure.com';
+        $platform = ($lms === 'canvas') ? 'canvas.instructure.com' : 'd2l.com';
 
         $customAppName = $request->query->get('tool_title');
         $default = $request->query->get('default');
