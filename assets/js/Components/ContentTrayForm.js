aa0889d3ba9fb14c81fabe4e3f53b251696dd832
Ethan  Finlay <ethanfinlay@MacBook-Pro.attlocal.net>
2021-06-17 12:55:15 -0400

    easyIssues filter now works with the issue dropdown properly

--- assets/js/Components/ContentTrayForm.js
+++ assets/js/Components/ContentTrayForm.js
@@ -32,7 +32,8 @@ class ContentTrayForm extends React.Component {
   }
 
   render() {
-console.log('render', this.props.filters)
+    console.log('render', this.props.filters)
+
     return (
       <Tray
         label={this.props.t('label.plural.filter')}
