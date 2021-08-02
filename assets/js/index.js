68fe7dd65b6aedb9acd5eff8b97fdf33cdd92263
AlanFCMV <verdu.alan@gmail.com>
2021-08-02 08:31:31 -0400

    Fix theme import

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Components/App'
 import getInitialData from './getInitialData'
+import { theme } from '@instructure/canvas-theme'
 
+theme.use()
 const data = getInitialData()
 
 ReactDOM.render(<App {...data} />, document.getElementById('root'))
