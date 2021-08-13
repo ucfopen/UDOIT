9a852194772dccdc7f10f803128cbd66a6afd2ad
Cristopher Matos <jasonmatos23@gmail.com>
2021-08-13 08:50:21 -0400

    Sets user table name to 'user' and updates composer and symfony lock files

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ORM\Table(name="users")
+ * @ORM\Table(name="user")
  */
 class User implements UserInterface, \Serializable, JsonSerializable
 {
