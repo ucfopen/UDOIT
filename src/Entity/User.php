2328170199b5235564d89f12f9420e92224a65cc
Cristopher Matos <jasonmatos23@gmail.com>
2021-08-20 13:12:58 -0400

    user to users in User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ORM\Table(name="user")
+ * @ORM\Table(name="users")
  */
 class User implements UserInterface, \Serializable, JsonSerializable
 {
