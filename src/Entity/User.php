355460e6a11cc551c4355b3d6243851bb8e9f9d6
Robert Boyd III <rob@robertcboyd.com>
2022-02-10 14:21:25 -0500

    Remove dead code from Entities

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use App\Services\UtilityService;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
