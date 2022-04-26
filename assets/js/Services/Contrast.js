2443416dbd580e86a47dad9e85d60feb88835d95
Robert Boyd III <ro962471@ucf.edu>
2022-04-26 10:03:04 -0400

    Convert function lambda to arrow function

--- assets/js/Services/Contrast.js
+++ assets/js/Services/Contrast.js
@@ -16,7 +16,7 @@ export function rgb2hex(rgb) {
 export function hexToRgb(hex) {
   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
   const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => {
     return r + r + g + g + b + b;
   });
 
