a4d56efc9ab328b9ceeaf627578690eeae15728e
Ahmad Altaher Alfayad <ahmadaltaher.alfayad@ucf.edu>
2023-07-05 09:36:58 -0400

    add skipif to new mysql migration file.

--- src/DoctrineMigrations/Version20230616193720.php
+++ src/DoctrineMigrations/Version20230616193720.php
@@ -19,12 +19,15 @@ final class Version20230616193720 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->skipIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE progress_bar ADD title VARCHAR(255) DEFAULT NULL');
+        
     }
 
     public function down(Schema $schema): void
     {
+        $this->skipIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE progress_bar DROP title');
     }
