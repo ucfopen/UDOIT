01f36c7d9d67f873de0711ced3dca18823bfc968
panbed <contact@bedp.dev>
2025-04-04 14:55:23 -0400

    small fix for making sure the checkboxes are correct when loading the form

--- assets/js/Components/Forms/StyleMisuseForm.js
+++ assets/js/Components/Forms/StyleMisuseForm.js
@@ -5,6 +5,8 @@ import { Button } from '@instructure/ui-buttons'
 import { IconCheckMarkLine } from '@instructure/ui-icons'
 import { Checkbox } from '@instructure/ui-checkbox'
 import { Spinner } from '@instructure/ui-spinner'
+import ProgressIcon from '../Icons/ProgressIcon'
+import ResolvedIcon from '../Icons/ResolvedIcon'
 import * as Html from '../../Services/Html'
 import * as Contrast from '../../Services/Contrast'
 
@@ -17,11 +19,11 @@ export default function StyleMisuseForm({
   handleActiveIssue,
   handleManualScan
 }) {
-  const [useBold, setUseBold] = useState(isBold())
-  const [useItalics, setUseItalics] = useState(isItalicized())
-  const [removeStyling, setRemoveStyling] = useState(false)
+  const [useBold, setUseBold] = useState(() => isBold())
+  const [useItalics, setUseItalics] = useState(() => isItalicized())
+  // const [removeStyling, setRemoveStyling] = useState(false)
   const [checkBoxErrors, setCheckBoxErrors] = useState([])
-  const [styleAttribute, setStyleAttribute] = useState(Html.getAttribute(Html.getIssueHtml(activeIssue), "style"))
+  // const [styleAttribute, setStyleAttribute] = useState(Html.getAttribute(Html.getIssueHtml(activeIssue), "style"))
 
   let formErrors = []
 
@@ -30,11 +32,15 @@ export default function StyleMisuseForm({
   }, [])
 
   useEffect(() => {
-    if (activeIssue?.issue) {
+    setUseBold(false)
+    setUseItalics(false)
+
+    if (activeIssue.status == '1') {
+      // set checkboxes based on if its already fixed
       setUseBold(isBold())
       setUseItalics(isItalicized())
       setCheckBoxErrors([])
-      setStyleAttribute(Html.getAttribute(Html.getIssueHtml(activeIssue), "style"))
+      // setStyleAttribute(Html.getAttribute(Html.getIssueHtml(activeIssue), "style"))
     }
 
     formErrors = []
@@ -42,7 +48,7 @@ export default function StyleMisuseForm({
 
   useEffect(() => {
     updatePreview()
-  }, [useBold, useItalics, removeStyling])
+  }, [useBold, useItalics])
 
   function handleBoldToggle() {
     setUseBold(!useBold)
@@ -54,10 +60,10 @@ export default function StyleMisuseForm({
     updatePreview()
   }
 
-  function handleStyleToggle() {
-    setRemoveStyling(!removeStyling)
-    updatePreview()
-  }
+  // function handleStyleToggle() {
+  //   setRemoveStyling(!removeStyling)
+  //   updatePreview()
+  // }
 
   function handleSubmit() {
     let issue = activeIssue
@@ -76,6 +82,18 @@ export default function StyleMisuseForm({
     }
   }
 
+  // function removeStylingAttribute(element) {
+  //   // remove styling recursively from all children (if they exist)
+  //   let children = element.children
+  //   if (children.length > 0) {
+  //     for (let i = 0; i < children.length; i++) {
+  //       removeStylingAttribute(children[i])
+  //     }
+  //   }
+
+  //   Html.removeAttribute(element, "style")
+  // }
+
   function processHtml(html) {
     let element = Html.toElement(html)
 
@@ -86,60 +104,48 @@ export default function StyleMisuseForm({
     element.innerHTML = (useBold) ? `<strong>${element.innerHTML}</strong>` : element.innerHTML
     element.innerHTML = (useItalics) ? `<em>${element.innerHTML}</em>` : element.innerHTML
 
-    if (removeStyling) {
-      Html.removeAttribute(element, "style")
-    }
-    else {
-      Html.setAttribute(element, "style", styleAttribute)
-    }
+    // if (removeStyling) {
+    //   removeStylingAttribute(element)
+    // }
+    // else {
+    //   Html.setAttribute(element, "style", styleAttribute)
+    // }
 
     return Html.toString(element)
   }
 
   function updatePreview() {
-    if (activeIssue) {
       let issue = activeIssue
       const html = Html.getIssueHtml(activeIssue)
   
       issue.newHtml = processHtml(html)
       handleActiveIssue(issue)
-    }
   }
 
   function isBold() {
-    if (activeIssue) {
-      const issue = activeIssue
-      const metadata = (issue.metadata) ? JSON.parse(issue.metadata) : {}
-      const html = Html.getIssueHtml(activeIssue)
-      const element = Html.toElement(html)
-
-      return ((Html.hasTag(element, 'strong')) || (metadata.fontWeight === 'bold'))
-    }
-    
-    return false
-  }
-
-  function isItalicized() {
-    if (activeIssue) {
-      const issue = activeIssue
-      const metadata = (issue.metadata) ? JSON.parse(issue.metadata) : {}
-      const html = Html.getIssueHtml(activeIssue)
-      const element = Html.toElement(html)
-  
-      return ((Html.hasTag(element, 'em')) || (metadata.fontStyle == 'italic'))
-    }
-
-    return false
-  }
-
-  function hasStyleTag() {
+    const issue = activeIssue
+    const metadata = (issue.metadata) ? JSON.parse(issue.metadata) : {}
     const html = Html.getIssueHtml(activeIssue)
     const element = Html.toElement(html)
 
-    return true
-
-    // return (Html.getAttribute(element, "style") != null)
+    return ((Html.hasTag(element, 'strong')) || (metadata.fontWeight === 'bold'))
   }
+
+  function isItalicized() {
+    const issue = activeIssue
+    const metadata = (issue.metadata) ? JSON.parse(issue.metadata) : {}
+    const html = Html.getIssueHtml(activeIssue)
+    const element = Html.toElement(html)
+
+    return ((Html.hasTag(element, 'em')) || (metadata.fontStyle == 'italic'))
+  }
+
+  // function hasStyleTag() {
+  //   const html = Html.getIssueHtml(activeIssue)
+  //   const element = Html.toElement(html)
+
+  //   return (Html.getAttribute(element, "style") != null)
+  // }
 
   function cssEmphasisIsValid(issue) {
     if (issue.scanRuleId === 'style_color_misuse') {
@@ -147,6 +153,7 @@ export default function StyleMisuseForm({
         return false
       }
     }
+
     return true
   }
 
@@ -156,44 +163,47 @@ export default function StyleMisuseForm({
   return (
     <div padding="0 x-small" className='pt-0 pb-0 pl-1 pr-1'>
       <div id="flash-messages" role="alert"></div>
-      <div className='mt-1 mb-1 ml-0 mr-0'>
-        <Checkbox label={t('form.contrast.bolden_text')}
-          checked={useBold}
-          onChange={handleBoldToggle}>
-        </Checkbox>
-        {/* <input id='bold-checkbox' type='checkbox' name={t('form.contrast.bolden_text')} />
-        <label htmlFor='bold-checkbox'>{t('form.contrast.bolden_text')}</label> */}
-      </div>
 
-      <View as="div" margin="small 0">
-        <Checkbox label={t('form.contrast.italicize_text')}
-          checked={useItalics}
-          onChange={handleItalicsToggle}
-          messages={checkBoxErrors}>
-        </Checkbox>
-      </View>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        handleSubmit()
+      }}>
+        <div className='mt-1 mb-1 ml-0 mr-0'>
+          <input id='bold-checkbox' type='checkbox' name={t('form.contrast.bolden_text')} checked={useBold} onChange={handleBoldToggle}/>
+          <label htmlFor='bold-checkbox'>{t('form.contrast.bolden_text')}</label>
+        </div>
 
-      <View as="div" margin="small 0">
-        {/* TOOD: use t */}
-        <Checkbox label={"Remove styling from element"}
-          checked={removeStyling}
-          onChange={handleStyleToggle}
-          messages={checkBoxErrors}>
-        </Checkbox>
-      </View>
+        <div className='mt-1 mb-1 ml-0 mr-0'>
+          <input id='italicize-checkbox' type='checkbox' name={t('form.contrast.italicize_text')} checked={useItalics} onChange={handleItalicsToggle}/>
+          <label htmlFor='italicize-checkbox'>{t('form.contrast.italicize_text')}</label>
+        </div>
 
-      <View as="div" margin="medium 0">
-        <Button color="primary" onClick={handleSubmit} interaction={(!pending && activeIssue?.status !== 2) ? 'enabled' : 'disabled'}>
-          {('1' == pending) && <Spinner size="x-small" renderTitle={buttonLabel} />}
-          {t(buttonLabel)}
-        </Button>
-        {activeIssue && activeIssue?.recentlyUpdated &&
-          <View margin="0 small">
-            <IconCheckMarkLine color="success" />
-            <View margin="0 x-small">{t('label.fixed')}</View>
-          </View>
+        <div className='mt-2 mb-1 ml-0 mr-0'>
+          <button className={(useBold === false && useItalics === false) || pending || activeIssue?.status === 2 ? 'btn btn-secondary disabled' : 'btn btn-primary'} disabled={pending || activeIssue?.status === 2 || useBold === false && useItalics === false} type='submit'>
+            {('1' == pending) && <div className='spinner'><ProgressIcon /></div>}
+            {t(buttonLabel)}
+          </button>
+          {activeIssue && activeIssue?.recentlyUpdated &&
+            <div className='mt-1 mb-1 ml-0 mr-0'>
+              <ResolvedIcon />
+              <span className='mt-1 mb-1 ml-0 mr-0'>{t('label.fixed')}</span>
+            </div>
+          }
+        </div>
+      </form>
+
+      {/* TODO: removeStyling would be cool IF IT WORKED */}
+      {/* <div className='mt-1 mb-1 ml-0 mr-0'>
+        <input id='styling-checkbox' type='checkbox' name={"Remove styling from element"} checked={removeStyling} onChange={handleStyleToggle}/>
+        <label htmlFor='styling-checkbox'>{"Remove styling from element"}</label>
+        {checkBoxErrors.forEach((error) => {
+          if (error.type === 'error') {
+            return <span className='text-danger'>{error.text}</span>
+          }
         }
-      </View>
+        )}
+      </div> */}
+
     </div>
   )
 }
