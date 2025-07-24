634fc7fca17a589cfeb7cb113811c0f66abbb35c
Daniel Molares <dm@ucf.edu>
2025-07-24 17:19:33 -0400

    Fixed typo in scope

--- src/Lms/Canvas/CanvasLms.php
+++ src/Lms/Canvas/CanvasLms.php
@@ -224,7 +224,7 @@ class CanvasLms implements LmsInterface {
                         'lmsContentId' => $lmsContent['id'],
                         'course' => $course,
                     ]);
-                    
+
                     if (!$force && $contentItem) {
                         $contentItemUpdated = $contentItem->getUpdated();
                         $lmsUpdated = new \DateTime($lmsContent['updated'], UtilityService::$timezone);
@@ -306,7 +306,7 @@ class CanvasLms implements LmsInterface {
         }
         else {
             $contentList = $response->getContent();
-            
+
             foreach ($contentList as $content) {
                 $formattedSection = [];
                 $formattedSection['id'] = $content['id'];
@@ -330,7 +330,7 @@ class CanvasLms implements LmsInterface {
                       throw new \Exception('msg.sync.error.api');
                   }
                   else {
-                    $itemList = $itemResponse->getContent();            
+                    $itemList = $itemResponse->getContent();
                     if(isset($itemList)) {
                         foreach ($itemList as $item) {
                             $formattedSection['items'][] = $item;
@@ -813,7 +813,7 @@ class CanvasLms implements LmsInterface {
 
             // Pages
             'url:GET|/api/v1/courses/:course_id/pages',
-            'url:GET|/api/v1/cour dses/:course_id/pages/:url_or_id',
+            'url:GET|/api/v1/courses/:course_id/pages/:url_or_id',
             'url:PUT|/api/v1/courses/:course_id/pages/:url_or_id',
 
             // Quiz Questions
