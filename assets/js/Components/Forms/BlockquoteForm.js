ad02a72cb9520c4be79f56ca512ade1e30587a10
Morgan Busch <morgan.busch@ucf.edu>
2025-08-04 14:18:19 -0400

    Blockquote form missing import.

--- assets/js/Components/Forms/BlockquoteForm.js
+++ assets/js/Components/Forms/BlockquoteForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import FormSaveOrReview from './FormSaveOrReview'
+import * as Text from '../../Services/Text'
 import * as Html from '../../Services/Html'
 
 export default function BlockquoteForm({
