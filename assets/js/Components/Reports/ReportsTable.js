f2c825460ef957bf5c01d74627acaaf98dfd65f2
Morgan Busch <morgan.busch@ucf.edu>
2025-09-10 09:42:15 -0400

    Updates the Report Screen to show the correct number of errors, fixed print screen discrepancies.

--- assets/js/Components/Reports/ReportsTable.js
+++ assets/js/Components/Reports/ReportsTable.js
@@ -9,9 +9,9 @@ export default function ReportsTable({
 
   const headers = [
     { id: "created", text: t('report.header.date') },
-    { id: "errors", text: t('report.header.issues'), alignText: 'center' },
-    { id: "potentialIssues", text: t('report.header.potential'), alignText: 'center' },
-    { id: "files", text: t('report.header.files_unreviewed'), alignText: 'center', divider: true },
+    { id: "knownBarriers", text: t('report.header.issues'), alignText: 'center' },
+    { id: "potentialBarriers", text: t('report.header.potential'), alignText: 'center' },
+    { id: "filesUnreviewed", text: t('report.header.files_unreviewed'), alignText: 'center', divider: true },
     { id: "contentHandled", text: t('report.header.items_handled'), alignText: 'center' },
     { id: "filesReviewed", text: t('report.header.files_reviewed'), alignText: 'center'}
   ]
@@ -35,14 +35,14 @@ export default function ReportsTable({
 
     list = list.map((report) => {
       if(report.scanCounts) {
-        report.issues = report.scanCounts.issues
-        report.potentialIssues = report.scanCounts.potentials
-        report.files = report.scanCounts.files || 0
+        report.knownBarriers = report.scanCounts.errors || 0
+        report.potentialBarriers = report.scanCounts.potentials
+        report.filesUnreviewed = report.scanCounts.files || 0
       }
       else {
-        report.issues = report.errors || 0
-        report.potentialIssues = 0
-        report.files = 0
+        report.knownBarriers = report.errors || 0
+        report.potentialBarriers = 0
+        report.filesUnreviewed = 0
       }
       report.contentHandled = report.contentFixed + report.contentResolved || 0
       return report
