d4f48a34f664cbe6ffe37cf53e6103eb2a997ca3
Morgan Busch <morgan.busch@ucf.edu>
2025-10-01 15:08:37 -0400

    Addresses failure to save when element is deleted.

--- src/Controller/IssuesController.php
+++ src/Controller/IssuesController.php
@@ -61,7 +61,7 @@ class IssuesController extends ApiController
 
             $contentUpdated = true;
             // Check if new HTML is different from original HTML
-            if ($newHtml !== '' && ($sourceHtml === $newHtml || $issue->getPreviewHtml() === $newHtml || $issue->getNewHtml() === $newHtml)) {
+            if ($sourceHtml === $newHtml || $issue->getPreviewHtml() === $newHtml || $issue->getNewHtml() === $newHtml) {
               $contentUpdated = false;
             }
 
