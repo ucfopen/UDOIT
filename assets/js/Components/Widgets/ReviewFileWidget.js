c76ae7e019ba0524b9e42b9dda381c71746ed6ec
Ishfaq Mohsin <ishfaq.mohsin@ucf.edu>
2026-01-05 14:06:47 -0500

    fix

--- assets/js/Components/Widgets/ReviewFileWidget.js
+++ assets/js/Components/Widgets/ReviewFileWidget.js
@@ -212,11 +212,6 @@ const ReviewFileWidget = (
 
   return (
     <div className='p-4 flex-column align-items-center justify-content-center'>
-        <div className='header-content flex-row w-100 justify-content-between align-items-baseline'>
-            <h3>Replace File</h3>
-            <CloseIcon tabIndex='0' />
-        </div>
-
         <FileAccessibilityInformation 
             activeFile={activeFile}
             getReadableFileSize={Text.getReadableFileSize}
@@ -242,58 +237,6 @@ const ReviewFileWidget = (
                 uploadedFile={uploadedFile}
             />
         </div>
-
-
-        {uploadedFile && <div className='file-ref-table-container w-100' id={markReview ? 'disabled' : ''}>
-            <div className='w-100 flex-row justify-content-between'>
-                <p>References in Course</p>
-                <p>{`${Object.keys(selectedRef).length} of ${fileReferenceHolder.length} references replaced`}</p>
-            </div>
-            <div className='table-wrapper'>
-            <table className='references-table'>
-                <thead>
-                    <tr>
-                        <th>Content</th>
-                        <th>Location</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {fileReferenceHolder?.map((ref, index) => (
-                        <tr key={index}>
-                            <td className='content-title'>
-                            <div className='flex-row align-items-center gap-1'>
-                                <input
-                                    type="checkbox"
-                                    className="custom-checkbox"
-                                    id="ref-checkbox"
-                                    checked={(ref.contentItemLmsId ? ref.contentItemLmsId : ref.itemId) in selectedRef}
-                                    onChange={() => handleReferenceSelect(ref)}
-                                    disabled={!uploadedFile}
-                                />
-                                <label htmlFor='ref-checkbox' className='ref-checkbox-label'>{ref.contentItemTitle}</label>
-                            </div>
-                            </td>
-                            <td>
-                                <a href={ref.contentItemUrl} target='_blank' className='location-link'>
-                                    External URL
-                                    <ExternalLinkIcon />
-                                </a>
-                            </td>
-                            <td>
-                                <span className="status-badge">
-                                    <span className="status-dot"></span>
-                                    {ref.status}
-                                </span>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            </div>
-        </div>}
-
-
     </div>
   )
 }
