7bd02630acca0a8602d18a7d7a272fdced6cfde6
Ishfaq Mohsin <ishfaq.mohsin@ucf.edu>
2026-01-08 13:41:32 -0500

    feat: accessiblity info box

--- assets/js/Components/Widgets/FileReviewPreview.js
+++ assets/js/Components/Widgets/FileReviewPreview.js
@@ -5,6 +5,8 @@ import ContentTypeIcon from '../Icons/ContentTypeIcon'
 import * as Text from '../../Services/Text'
 import './FixIssuesContentPreview.css'
 import SeverityIcon from '../Icons/SeverityIcon'
+import ContentPageIcon from '../Icons/ContentPageIcon'
+
 
 export default function FixIssuesContentPreview({
   t,
@@ -13,10 +15,16 @@ export default function FixIssuesContentPreview({
 }) {
 
   const [fileReferenceHolder, setFileReferenceHolder] = useState([])
+  const [currentFile, setCurrentFile] = useState(null)
 
   useEffect(() => {
     if(activeIssue){
       handleFileReference()
+      if(activeIssue.fileData.replacement){
+        setCurrentFile(activeIssue.fileData.replacement)
+        return
+      }
+      setCurrentFile(activeIssue.fileData)
     }
   }, [activeIssue])
 
@@ -74,6 +82,37 @@ export default function FixIssuesContentPreview({
 
   return (
     <>
+     { currentFile && ( 
+      <div className='file-accessibility-info-wrapper w-100 p-2'>
+        <div className='accessibility-info-container flex-column w-100'>
+          <div className='file-info p-2'>
+              <div className='current-file-container flex-row gap-1 align-items-center'>
+                <p className='current-file-tag'>Current File</p>
+                <span className="status-badge">{activeIssue.fileData?.replacement ? "New" : "Original"}</span>
+              </div>
+          </div>
+
+          <div className='flex-row gap-1 align-items-start p-2'>
+            <ContentPageIcon className='file-icon icon-md p-2' />
+             <div className='flex-column'>
+              <div className='file-name flex-row align-items-center gap-1'>
+                  <a href={currentFile.lmsUrl} target='_blank' className='file-name-link'>{currentFile.fileName}</a>
+                  <ExternalLinkIcon className="icon-sm link-color" />
+              </div>
+              <div className='file-details flex-row gap-1'>
+                <div>File Type: <span className='fw-bold'>{getReadableFileType(currentFile.fileType)}</span> </div>
+                <div>File Size: <span className='fw-bold'>{Text.getReadableFileSize(currentFile.fileSize)}</span> </div>
+              </div>
+             </div>
+          </div>
+            <div className='accessibility-instructions-container w-100 flex-row p-2 justify-content-between'>
+                <p>Ensure this file meets accessibility standards for PDFs (proper headings, alt text)w</p>
+                <button className='accessibility-btn'>Show Accessibility Guidelines</button>
+            </div>
+        </div>
+      </div>)
+      }
+
      <div className="mt-3 rounded-table-wrapper">
       <table className="file-reference-table">
       <thead>
