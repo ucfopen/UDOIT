2bd1869e0323eeea57114fbfc0dfe79347dfe17b
Ishfaq Mohsin <ishfaq.mohsin@ucf.edu>
2026-01-14 15:28:19 -0500

    feat: showing file status on reviewed and unreviewed

--- assets/js/Components/Widgets/FileFixitWidget.js
+++ assets/js/Components/Widgets/FileFixitWidget.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import BarrierInformation from './BarrierInformation'
 import FileForm from '../Forms/FileForm'
 import './UfixitWidget.css'
+import CheckIcon from '../Icons/CheckIcon'
+import SeverityPotentialIcon from '../Icons/SeverityPotentialIcon'
+import SeverityIssueIconFilled from '../Icons/SeverityIssueIconFilled'
 
 
 export default function FileFixitWidget({
@@ -23,7 +26,13 @@ export default function FileFixitWidget({
   return (
     <div className="ufixit-widget flex-column flex-grow-1">
        <div className="flex-column gap-1">
+        <div className='review-status flex-row gap-1 align-items-center'>
           <h3>Review File</h3>
+          <div className={`file-review-status-container ${tempActiveIssue.fileData.reviewed ? 'file-reviewed' : 'file-progress'} flex-row align-items-center p-2 gap-1`}>
+              {tempActiveIssue.fileData.reviewed ? <CheckIcon fill={'var(--primary-color)'} /> : <SeverityIssueIconFilled fill={'var(--potential-color)'} />}
+              <div>{tempActiveIssue.fileData.reviewed ? 'Reviewed' : 'Needs Review'}</div>
+          </div>
+        </div>
           <FileForm
             t={t}
             settings={settings}
