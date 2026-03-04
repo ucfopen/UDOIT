b66a2fe7e8ae6e625b077dbb04dd3a187afcc29c
Ahmad Altaher Alfayad <taheralfayad@gmail.com>
2026-03-04 11:10:33 -0500

    feat(database): add account and term tables

---
+++ src/DoctrineMigrations/Version20260304160904.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260304160904 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE account (lms_account_id VARCHAR(255) NOT NULL, institution_id INT NOT NULL, account_name VARCHAR(255) NOT NULL, INDEX IDX_7D3656A410405986 (institution_id), PRIMARY KEY(lms_account_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE term (lms_term_id VARCHAR(255) NOT NULL, institution_id INT NOT NULL, term_name VARCHAR(255) NOT NULL, INDEX IDX_A50FE78D10405986 (institution_id), PRIMARY KEY(lms_term_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE account ADD CONSTRAINT FK_7D3656A410405986 FOREIGN KEY (institution_id) REFERENCES institution (id)');
+        $this->addSql('ALTER TABLE term ADD CONSTRAINT FK_A50FE78D10405986 FOREIGN KEY (institution_id) REFERENCES institution (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE account DROP FOREIGN KEY FK_7D3656A410405986');
+        $this->addSql('ALTER TABLE term DROP FOREIGN KEY FK_A50FE78D10405986');
+        $this->addSql('DROP TABLE account');
+        $this->addSql('DROP TABLE term');
+    }
+}
