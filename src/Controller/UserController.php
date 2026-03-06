b385b718d24eec9ed66b469bf8284a37cc763089
panbed <contact@bedp.dev>
2026-03-06 17:05:08 -0500

    change usercontroller so that user can only change their own user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Response\ApiResponse;
+use App\Services\SessionService;
 use App\Services\UtilityService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class UserController extends AbstractController
 {
@@ -23,37 +26,63 @@ class UserController extends AbstractController
     }
 
     #[Route('/api/users/{user}', name: 'user_put',  methods: ['PUT'])]
-    public function update(User $user, Request $request, UtilityService $util): JsonResponse
+    public function update(SessionService $sessionService, User $user, Request $request, UtilityService $util): JsonResponse
     {
-        $userVals = \json_decode($request->getContent(), true);
-        $oldRoles = $user->getRoles();
-        $oldLang = $oldRoles['lang'] ?? 'en';
+        $apiResponse = new ApiResponse();
+        $output = new ConsoleOutput();
+        $responseObject = null;
         
-        $newRoles = $userVals['roles'] ?? [];
-        $newLang = $newRoles['lang'] ?? 'en';
+        try {
+            // Check if user is updating their own info
+            $userIdFromSession = $sessionService->getSession()->get('userId');
 
-        $changeLang = ($oldLang !== $newLang);
+            $output->writeln(json_encode($userIdFromSession));
+            $output->writeln(json_encode($user->getId()));
+            if ($user->getId() !== $userIdFromSession) {
+                throw new \Exception("msg.no_permissions");
+            }
 
-        $user->setRoles($newRoles);
-        if (empty($userVals['hasApiKey'])) {
-          $user->setApiKey('');
-          $user->setRefreshToken('');
+            $userVals = \json_decode($request->getContent(), true);
+
+            
+            $oldRoles = $user->getRoles();
+            $oldLang = $oldRoles['lang'] ?? 'en';
+            
+            $newRoles = $userVals['roles'] ?? [];
+            $newLang = $newRoles['lang'] ?? 'en';
+
+            $changeLang = ($oldLang !== $newLang);
+
+            $user->setRoles($newRoles);
+            if (empty($userVals['hasApiKey'])) {
+            $user->setApiKey('');
+            $user->setRefreshToken('');
+            }
+
+            $responseObject = [
+                'user' => $user,
+                'language' => $newLang,
+                'labels' => [],
+            ];
+
+            if ($changeLang) {
+                $this->util = $util;
+                $labels = $this->util->getTranslation($newLang);
+                $responseObject['labels'] = $labels;
+            }
+
+            $this->doctrine->getManager()->flush();
+
+            $apiResponse->setData($responseObject);
+        }
+        catch (\Exception $e) {
+            $apiResponse->addError($e->getMessage());
         }
 
-        $responseObject = [
-            'user' => $user,
-            'language' => $newLang,
-            'labels' => [],
-        ];
-
-        if ($changeLang) {
-          $this->util = $util;
-          $labels = $this->util->getTranslation($newLang);
-          $responseObject['labels'] = $labels;
+        if ($responseObject === null) {
+            return new JsonResponse($apiResponse);
         }
 
-        $this->doctrine->getManager()->flush();
-
-        return $this->json($responseObject);
+        return new JsonResponse($responseObject);
     }
 }
