023c7a96bbbbabe42cb79dafa9d1563ff06a9198
Elias Wainberg <elias.wainberg@ucf.edu>
2026-03-31 15:23:08 -0400

    Ignores eslint prop-type rule and test lints+pretties file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,14 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
 ]);
