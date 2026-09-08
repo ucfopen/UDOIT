73312820408e3f302c8270f8839a246cc778315b
Ishfaq Mohsin <ishfaq.mohsin@ucf.edu>
2026-09-08 13:30:54 -0400

    chore(formatting): formatted DashboardCourseTable widget

--- assets/js/Components/Widgets/DashboardCourseTable.js
+++ assets/js/Components/Widgets/DashboardCourseTable.js
@@ -20,35 +20,35 @@ const DashboardCourseTable = ({t, courses}) => {
 
   
     const sortContent = () => {
-    let tempRows = courses ? courses.map((course) => ({
-      ...course,
-      scanRule: t(`form.${formNameFromRule(course.scanRule)}.title`),
-      lastUpdated: (new Date(course.lastUpdated)).toDateString(),
-    })) : []
+      let tempRows = courses ? courses.map((course) => ({
+        ...course,
+        scanRule: t(`form.${formNameFromRule(course.scanRule)}.title`),
+        lastUpdated: (new Date(course.lastUpdated)).toDateString(),
+      })) : []
 
-    const { sortBy, ascending } = tableSettings
+      const { sortBy, ascending } = tableSettings
 
-    tempRows.sort((a, b) => {
-      let aValue = a[sortBy] ?? ''
-      let bValue = b[sortBy] ?? ''
+      tempRows.sort((a, b) => {
+        let aValue = a[sortBy] ?? ''
+        let bValue = b[sortBy] ?? ''
 
-      if (typeof(aValue) === "object" && typeof(bValue) === "object") {
-        aValue = a[sortBy + "_display"] || ""
-        bValue = b[sortBy + "_display"] || ""
+        if (typeof(aValue) === "object" && typeof(bValue) === "object") {
+          aValue = a[sortBy + "_display"] || ""
+          bValue = b[sortBy + "_display"] || ""
+        }
+
+        if (isNaN(aValue) || isNaN(bValue)) {
+          return (String(aValue).toLowerCase() > String(bValue).toLowerCase()) ? -1 : 1
+        } else {
+          return (Number(aValue) < Number(bValue)) ? -1 : 1
+        }
+      })
+
+      if (!ascending) {
+        tempRows.reverse()
       }
 
-      if (isNaN(aValue) || isNaN(bValue)) {
-        return (String(aValue).toLowerCase() > String(bValue).toLowerCase()) ? -1 : 1
-      } else {
-        return (Number(aValue) < Number(bValue)) ? -1 : 1
-      }
-    })
-
-    if (!ascending) {
-      tempRows.reverse()
-    }
-
-    return tempRows
+      return tempRows
   }
 
    useEffect(() => {
